b26077c6b7125db3471b61c90206d396ae214c7c
Admin <Admin@MD-Kabir>
2017-01-28 13:54:09 -0500

    Initial Checkin

---
+++ Java_Module_2/src/Boolean_Datatype.java
@@ -0,0 +1,65 @@
+
+public class Boolean_Datatype {
+
+	public static void main(String[] args) {
+		/*boolean bol = true;
+		System.out.println(10<20);
+		
+		int first_no = 10;
+		int sec_no = 20;
+		int third_no = 30;
+		
+		String abc= "wellcome to selenium tutorial";
+		String def= "wellcome to selenium tutorial";
+		
+		if (abc ==def){
+			System.out.println("Both String are equal");
+		}else{
+			System.out.println("Both are not equal");
+		}
+		
+		
+		
+		boolean result = first_no>sec_no;
+		System.out.println(result);
+		
+		if (first_no>sec_no){
+			System.out.println("first no is greater" + first_no);
+		}else if(sec_no>first_no && sec_no>third_no){
+			System.out.println("second no is greater" + sec_no);
+		}else{
+			System.out.println("third_no is greater" + third_no);
+		}
+		*/
+		boolean b = true;
+		System.out.println(10<20);
+		
+		int first_no =10;
+		int sec_no = 20;
+		int third_no = 30;
+		
+		String abc = "this is true";
+				String efg = "this is  true";
+				
+				if (abc==efg){
+				
+				System.out.println("This is Kabir");
+				}else{
+				System.out.println("This is not kabir");	
+				}
+				
+				boolean result = first_no>sec_no;
+				System.out.println(result);
+				
+				if(first_no>sec_no){
+					System.out.println("first no is greater" + first_no);
+				}else if (sec_no>first_no && sec_no>third_no){
+					System.out.println("second no is greater" + sec_no);
+				}else{
+					System.out.println("third no is greateer" + third_no);
+				}
+		
+	}
+
+
+}
---
+++ Java_Module_2/src/Switch_Example.java
@@ -0,0 +1,54 @@
+
+public class Switch_Example {
+
+	public static void main(String[] args) {
+
+
+		int month = 16;
+		
+		switch(month){
+		
+		case 1:
+		System.out.println("jan");
+		break;
+		case 2:
+		System.out.println("feb");
+		break;
+		case 3:
+		System.out.println("mar");
+		break;
+		case 4:
+		System.out.println("apr");
+		break;
+		case 5:
+		System.out.println("may");
+		break;
+		case 6:
+		System.out.println("jun");
+		break;
+		case 7:
+		System.out.println("july");
+		break;
+		case 8:
+		System.out.println("Aug");
+		break;
+		case 9:
+		System.out.println("sep");
+		break;
+		case 10:
+		System.out.println("oct");
+		break;
+		case 11:
+		System.out.println("Nov");
+		break;
+		case 12:
+		System.out.println("Dec");
+		
+		
+		
+		
+		}
+		
+	}
+
+}
